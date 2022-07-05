d164fc1f4a8a1e218870277d3a9ddf63e1327091
Igor Turko <igor@sharkpunch.com>
2022-07-05 16:48:23 +0300

    - React router added; - Dummy ToDoPage added; - Unit tests updated;

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import * as C from '@chakra-ui/react';
-
 import { createStandaloneToast, extendTheme } from '@chakra-ui/react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import ToDoPage from './components/ToDoPage';
 
 const theme = {
   config: {
@@ -16,6 +18,11 @@ export function MainContainer() {
 
   return (
     <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ToDoPage />}></Route>
+        </Routes>
+      </BrowserRouter>
       <ToastContainer />
     </>
   );
---
+++ src/components/ToDoPage.tsx
@@ -0,0 +1,11 @@
+import * as C from '@chakra-ui/react';
+
+const ToDoPage = () => {
+  return (
+    <C.Container maxW="container.xl" h="calc(100vh - 4rem)">
+      Todo list:
+    </C.Container>
+  );
+};
+
+export default ToDoPage;
